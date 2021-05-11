4cfa20a2bc1e7d8e45e4b77ce2b4dc60a3b2a3bb
Umer Nasir <umernasirr@gmail.com>
2021-05-12 03:22:36 +0500

    styled home page

--- App.js
+++ App.js
@@ -4,37 +4,53 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from "expo-font";
 
-import { Provider as PaperProvider } from "react-native-paper";
+import {
+  ActivityIndicator,
+  Provider as PaperProvider,
+  DefaultTheme,
+} from "react-native-paper";
 
 // Screens
 import Home from "./src/screens/Home";
-import Settings from "./src/screens/Settings";
 import Login from "./src/screens/Login";
 import Register from "./src/screens/Register";
+import { Colors } from "./src/helpers/theme";
 
 // TODO: Install a library material bottom navigator
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Colors.primary,
+    accent: Colors.secondary,
+  },
+};
+
 export default function App() {
+  const [loaded] = useFonts({
+    LemonMilk: require("./assets/fonts/LEMONMILK-Medium.otf"),
+  });
+
   const Stack = createStackNavigator();
 
-  return (
+  return loaded ? (
     <NavigationContainer>
-      <PaperProvider>
-        <Stack.Navigator>
+      <PaperProvider theme={theme}>
+        <Stack.Navigator headerMode="none" initialRouteName="Home">
           <Stack.Screen name="Register" component={Register} />
 
           <Stack.Screen name="Login" component={Login} />
 
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
-
-        {/* <Home />
-        <Settings /> */}
-        {/* <Login />
-        <Register /> */}
       </PaperProvider>
     </NavigationContainer>
+  ) : (
+    <ActivityIndicator />
   );
 }
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -11,5 +11,59 @@ export const users = [
   },
 ];
 
-// TODO: ASBAH:
-export const menu = [];
+export const categories = [
+  {
+    id: 1,
+    name: "Pizza",
+    icoName: "pizza",
+  },
+  {
+    id: 2,
+    name: "Seafood",
+    icoName: "fish",
+  },
+  {
+    id: 3,
+    name: "Soft Drinks",
+    icoName: "food-fork-drink",
+  },
+
+  {
+    id: 4,
+    name: "Grilled",
+    icoName: "food-steak",
+  },
+];
+
+export const specialItems = [
+  {
+    id: 1,
+    name: "Pasta Pesto",
+    price: 60,
+    weight: 128,
+    img: require("../../assets/images/pasta.png"),
+  },
+  {
+    id: 2,
+    name: "Primavera Pizza",
+    price: 25,
+    weight: 210,
+    img: require("../../assets/images/pizza.png"),
+  },
+  {
+    id: 3,
+    name: "Pepsi Coke",
+    price: 57,
+    weight: 23,
+    img: require("../../assets/images/pepsi.png"),
+  },
+
+  {
+    id: 4,
+    name: "Grilled",
+    icoName: "food-steak",
+    price: 23,
+    weight: 80,
+    img: require("../../assets/images/grilled.png"),
+  },
+];
