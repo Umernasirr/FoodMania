02618fdeb6677c79000b1ca7d920ad07eece7087
ZaharaQue <zaharaquresh@gmail.com>
2021-05-16 20:06:48 +0500

    Setting screen setup, incomplete

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { users } from "../constants";
+import { Switch } from "react-native-paper";
 import { Colors, globalStyles } from "../helpers/theme";
 const Settings = () => {
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+
   return (
     <View style={globalStyles.container}>
-      <Text>Settings</Text>
+      <Text style={styles.heading}>Settings</Text>
+      <Text>Name: Umer Nasir </Text>
+      <Text>Email: umeri@nasir.com </Text>
+      <Text>
+        Notification:{" "}
+        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />{" "}
+      </Text>
     </View>
   );
 };
 
 export default Settings;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+});
